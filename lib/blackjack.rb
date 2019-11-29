c913c1de85b884733c24dfd4f572d3e523893c89
Almmond Hudnall <ahudnall24@gmail.com>
2019-11-29 21:55:04 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -77,3 +77,12 @@ describe "#welcome" do
     .welcome
   end
     
+describe "#display_card_total" do
+  it "accepts one argument, the card total" do
+    expect { display_card_total(7) }.to_not raise_error
+  end
+ 
+  it "prints the value of the cards to the screen" do
+    expect($stdout).to receive(:puts).with("Your cards add up to 8")
+    display_card_total(8)
+  end
